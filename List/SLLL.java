9fe688f8d5890ebeebcb36837c02b2487018103d
Ankush Gupta <ankushgupta365@gmail.com>
2022-10-13 19:07:36 +0530

    detect loop in linked list method added which uses two pointer approach

--- List/SLLL.java
+++ List/SLLL.java
@@ -219,6 +219,37 @@ public class SLLL {
         if(current == null)return;
         temp.next = current.next;
     }
+    public Boolean detectLoopInLinkedlist(){
+        if(head == null){
+            return false;
+        }
+        ListNode fastPtr = head;
+        ListNode slowPtr = head;
+        //if there is a loop then definitely there will come a situation when slowptr and fastptr going to meet
+        while(fastPtr!=null && fastPtr.next !=null){
+            fastPtr = fastPtr.next.next;
+            slowPtr= slowPtr.next;
+            if(slowPtr==fastPtr){
+                return true;
+            }
+        }
+        return false;
+    }
+    public void createLoopInLinkedList(){
+        ListNode first = new ListNode(1);
+        ListNode second = new ListNode(2);
+        ListNode third = new ListNode(3);
+        ListNode fourth = new ListNode(4);
+        ListNode fifth = new ListNode(5);
+        ListNode sixth = new ListNode(6);
+        head = first;
+        first.next = second;
+        second.next = third;
+        third.next = fourth;
+        fourth.next = fifth;
+        fifth.next = sixth;
+        sixth.next = third; // this will create a loop where 6th point to third
+    }
     public static void main(String[] args) {
         SLLL sll = new SLLL();
         // sll.head = new ListNode(20);
@@ -254,14 +285,18 @@ public class SLLL {
         // sll.display(reversedList);
         // sll.removeDuplicateFromSorted(reversedList);
         // sll.display(reversedList);
-        sll.insertFirst(60);
-        sll.insertFirst(50);
-        sll.insertFirst(40);
-        sll.insertFirst(30);
-        sll.display(sll.head);
-        sll.insertInSortedList(sll.head, 45);
-        sll.display(sll.head);
-        sll.removeGivenKey(45);
-        sll.display(sll.head);
+
+        // sll.insertFirst(60);
+        // sll.insertFirst(50);
+        // sll.insertFirst(40);
+        // sll.insertFirst(30);
+        // sll.display(sll.head);
+        // sll.insertInSortedList(sll.head, 45);
+        // sll.display(sll.head);
+        // sll.removeGivenKey(45);
+        // sll.display(sll.head);
+        
+        sll.createLoopInLinkedList();
+        System.out.println("do linked list have loop: " + sll.detectLoopInLinkedlist());
     }
 }
