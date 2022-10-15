271691b07c014b31b45631be95783e6e4a5afff4
Ankush Gupta <ankushgupta365@gmail.com>
2022-10-15 12:41:46 +0530

    leetcode21 solution added using dummy list concept

--- List/SLLL.java
+++ List/SLLL.java
@@ -350,3 +350,40 @@ public class SLLL {
 
     }
 }
+
+///// leetcode questions ///
+
+class LeetCode21{
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(){};
+        ListNode(int val){
+            this.val = val;
+        }
+        ListNode(int val, ListNode next){
+            this.val = val;
+            this.next = next;
+        }
+    }
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(-101);
+        ListNode tail = dummy;
+        while(list1 != null && list2 != null){
+            if(list1.val <= list2.val){
+                tail.next = list1;
+                list1 = list1.next;
+            }else{
+                tail.next = list2;
+                list2 = list2.next;
+            }
+            tail = tail.next;
+        }
+        if(list1 != null){
+            tail.next = list1;
+        }else{
+            tail.next = list2;
+        }
+        return dummy.next;
+    }
+}
