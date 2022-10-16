eace874ccdabe86688b63ea053a392f6a7cb435e
Ankush Gupta <86787266+ankushgupta365@users.noreply.github.com>
2022-10-16 22:35:40 +0530

    Time: 1 ms (80.82%), Space: 41.2 MB (81.69%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -3,20 +3,18 @@ class Solution {
         if(numRows<=0)
             return new ArrayList();
         List<List<Integer>> result = new ArrayList();
-         List<Integer> pre = null;
         for(int i=0; i<numRows; i++){
             List<Integer> row = new ArrayList();
-           
+           //2nd row 2 col, 3rd row have 3col and so on.. 
             for(int j=0; j<=i; j++){
-                if(j==0 || j==i){
+                if(j==0 || j==i){ //since first and last elem of row have 1
                     row.add(1);
                 }else{
+                    //since every element other then first and last of row is sum of it's                           [row-1][j-1] + [row][j] elem
                     row.add(result.get(i-1).get(j-1) + result.get(i-1).get(j));
-                    // row.add(pre.get(j-1)+pre.get(j));
                 }
                
             }
-            pre = row;
              result.add(row);
         }
         return result;
