dab23553dcd2c08251d611d06ec017f0a781f199
Devanshurai111 <devanshurai11@gmail.com>
2022-10-19 20:00:05 +0530

    Program of reverse Array

---
+++ Reverse-Array/ArrayReverse.java
@@ -0,0 +1,22 @@
+package Reverse;
+
+import java.util.*;
+public class ArrayReverse {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        System.out.println("Original Array: " + Arrays.toString(arr));
+        reverse(arr);
+        System.out.println("Reversed Array: " + Arrays.toString(arr));
+    }
+    public static void reverse(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
